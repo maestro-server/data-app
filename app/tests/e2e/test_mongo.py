84ec03b9fba2238e287d22fc379ac25962586efc
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-06-13 23:39:49 +0000

    bugfix - test

--- app/tests/e2e/test_mongo.py
+++ app/tests/e2e/test_mongo.py
@@ -17,9 +17,9 @@ class MongoTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_main_page(self):
-        response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+    def test_simple_request(self):
+        response = self.app.get('/servers?query=%27{}%27', follow_redirects=True)
+        self.assertEqual(response.status_code, 422)
 
 
 if __name__ == "__main__":
