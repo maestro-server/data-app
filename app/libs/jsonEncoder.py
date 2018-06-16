33ed390dc62bf1aa7f329082fd5d1503c093f8dd
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-06-16 13:20:40 -0300

    bugfix - jsondecoder - empty val

--- app/libs/jsonEncoder.py
+++ app/libs/jsonEncoder.py
@@ -24,7 +24,7 @@ class DateTimeEncoder(json.JSONEncoder):
                 val = obj.decode('utf-8')
             except Exception as err:
                 logger.error("==================================> Decode is not utf-8 - %s", str(err))
-                return val
+                pass
 
         if val is None:
             val = json.JSONEncoder.default(self, obj)
