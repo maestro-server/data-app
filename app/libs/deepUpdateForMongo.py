70fd180806bd53bcd883dbe3fc15be5ca462769c
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-07-17 09:51:27 -0300

    bugfix - false positive to find _id object id fields

--- app/libs/deepUpdateForMongo.py
+++ app/libs/deepUpdateForMongo.py
@@ -7,8 +7,10 @@ def updaterIds(data, path):
     if isinstance(last, int):
         last = ".".join(map(str, path))
 
-    if isinstance(last, str):
+    if isinstance(last, str) and len(str(data)) == 24:
         data = Ruler.searchID(last, data)
+
+    if isinstance(last, str) and len(str(data)) > 24:
         data = Ruler.searchAt(last, data)
 
     return data
